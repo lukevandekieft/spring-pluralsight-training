ac67ba31f669042a4fba4b0cb583b1a73071683b
lukevandekieft <vandekie@gmail.com>
2021-05-26 13:41:08 -0700

    Explicitly create singleton service.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -11,6 +11,11 @@ public class Application {
     // SpeakerService service = new SpeakerServiceImpl();     <-- old hard-coded way. Example below using setter injection.
     SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
+    // 3 lines below prove that SpeakerService is singleton - both instances have the same object address
+    System.out.println(service);
+    SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+    System.out.println(service2);
+
     System.out.println(service.findAll().get(0).getFirstName());
     System.out.println(service.findAll().get(0).getLastName());
   }
