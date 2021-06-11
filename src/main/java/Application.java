c5152a498e5647609cb67ba63783161a5bc58740
lukevandekieft <vandekie@gmail.com>
2021-06-11 08:34:25 -0700

    Add new beans to XML.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,10 +3,14 @@ import com.pluralsight.service.SpeakerServiceImpl;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.applet.AppletContext;
 
 public class Application {
   public static void main(String args[]) {
-     SpeakerService service = new SpeakerServiceImpl();
+    ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+    SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
     System.out.println(service.findAll().get(0).getFirstName());
     System.out.println(service.findAll().get(0).getLastName());
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,18 +1,17 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
-  private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+  private SpeakerRepository repository;
+
+  public void setSpeakerRepository(SpeakerRepository repository) {
+    this.repository = repository;
+  }
 
   public List<Speaker> findAll() {
     return repository.findAll();
