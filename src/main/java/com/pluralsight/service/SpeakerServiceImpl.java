f4cf39fc033c05d0c8a621cf86f79a47129789bf
lukevandekieft <vandekie@gmail.com>
2021-06-17 16:33:02 -0700

    Add new library and PostConstruct

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -5,16 +5,24 @@ import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 public class SpeakerServiceImpl implements SpeakerService {
 
   private SpeakerRepository repository;
 
   public SpeakerServiceImpl() {
-
+    System.out.println("repository Constructor");
   }
 
   public SpeakerServiceImpl(SpeakerRepository repository) {
     this.repository = repository;
+  }
+
+  // Doesn't work in XML: need to be Java-based since the container isn't managed here
+  @PostConstruct
+  private void initialize() {
+    System.out.println("Called after constructors");
   }
 
   public void setSpeakerRepository(SpeakerRepository repository) {
