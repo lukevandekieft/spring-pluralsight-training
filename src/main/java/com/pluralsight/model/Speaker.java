5be8313c2a90cead8d0cba5381932b6a2bf1bfcc
lukevandekieft <vandekie@gmail.com>
2021-06-17 18:12:50 -0700

    Add SpEL.

--- src/main/java/com/pluralsight/model/Speaker.java
+++ src/main/java/com/pluralsight/model/Speaker.java
@@ -3,9 +3,18 @@ package com.pluralsight.model;
 public class Speaker {
   private String firstName;
   private String lastName;
+  private Double seedNum;
 
   public String getFirstName() {
     return firstName;
+  }
+
+  public Double getSeedNum() {
+    return seedNum;
+  }
+
+  public void setSeedNum(Double seedNum) {
+    this.seedNum = seedNum;
   }
 
   public void setFirstName(String firstName) {
--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.pluralsight.repository;
 import com.pluralsight.model.Speaker;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -15,12 +16,16 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
   @Autowired
   private Calendar cal;
 
+  @Value("#{ T(java.lang.Math).random() * 100 }")
+  private double seedNum;
+
   public List<Speaker> findAll() {
     List<Speaker> speakers = new ArrayList<Speaker>();
 
     Speaker speaker = new Speaker();
     speaker.setFirstName("Luke");
     speaker.setLastName("Vandekieft");
+    speaker.setSeedNum(seedNum);
 
     System.out.println("cal: " + cal.getTime());
 
