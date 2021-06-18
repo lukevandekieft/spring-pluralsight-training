36c0dae1ca416813ea39c555cdc631bdb5a08322
lukevandekieft <vandekie@gmail.com>
2021-06-17 18:07:49 -0700

    Create Factory Bean.

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -2,20 +2,27 @@ package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
-  @Override
+
+  @Autowired
+  private Calendar cal;
+
   public List<Speaker> findAll() {
     List<Speaker> speakers = new ArrayList<Speaker>();
 
     Speaker speaker = new Speaker();
     speaker.setFirstName("Luke");
     speaker.setLastName("Vandekieft");
+
+    System.out.println("cal: " + cal.getTime());
 
     speakers.add(speaker);
 
