67a305e3122cafdae7c9f0582003c6edb962406a
lukevandekieft <vandekie@gmail.com>
2021-06-24 11:14:13 -0700

    Create controllers.

---
+++ src/main/java/com/pluralsight/controllers/SessionsController.java
@@ -0,0 +1,37 @@
+package com.pluralsight.controllers;
+
+import com.pluralsight.models.Session;
+import com.pluralsight.repositories.SessionRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionsController {
+  @Autowired
+  private SessionRepository sessionRepository;
+
+  @GetMapping
+  public List<Session> list() {
+    return sessionRepository.findAll();
+  }
+
+  @GetMapping
+  @RequestMapping("{id}")
+  public Session get(@PathVariable Long id) {
+    return sessionRepository.getOne(id);
+  }
+
+  @PostMapping
+  public Session create(@RequestBody final Session session) {
+    return sessionRepository.saveAndFlush(session);
+  }
+}
