2648c6892a29dfed0935fb8747f33731c9283e6f
lukevandekieft <vandekie@gmail.com>
2021-06-24 12:48:01 -0700

    Add CRUD functions.

--- src/main/java/com/pluralsight/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/controllers/SessionsController.java
@@ -3,12 +3,14 @@ package com.pluralsight.controllers;
 import com.pluralsight.models.Session;
 import com.pluralsight.repositories.SessionRepository;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -33,5 +35,21 @@ public class SessionsController {
   @PostMapping
   public Session create(@RequestBody final Session session) {
     return sessionRepository.saveAndFlush(session);
+  }
+
+  // No such thing as DeleteMapping, hence RequestMapping
+  @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+  public void delete(@PathVariable Long id) {
+    // Also need to check for children records before deleting. In real implementation we'd have orphaned info as-is
+    sessionRepository.deleteById(id);
+  }
+
+  // Consider PATCH if I'm only trying to replace certain fields
+  @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+  public Session update(@PathVariable Long id, @RequestBody Session session) {
+    // in production we'd want to verify body attributes
+    Session existingSession = sessionRepository.getOne(id);
+    BeanUtils.copyProperties(session, existingSession, "session_id");
+    return sessionRepository.saveAndFlush(existingSession);
   }
 }
--- src/main/java/com/pluralsight/controllers/SpeakersController.java
+++ src/main/java/com/pluralsight/controllers/SpeakersController.java
@@ -3,10 +3,14 @@ package com.pluralsight.controllers;
 import com.pluralsight.models.Speaker;
 import com.pluralsight.repositories.SpeakerRepository;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,5 +30,22 @@ public class SpeakersController {
   @RequestMapping("{id}")
   public Speaker get(@PathVariable Long id) {
     return speakerRepository.getOne(id);
+  }
+
+  @PostMapping
+  public Speaker create(@RequestBody final Speaker speaker) {
+    return speakerRepository.saveAndFlush(speaker);
+  }
+
+  @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+  public void delete(@PathVariable Long id) {
+    speakerRepository.deleteById(id);
+  }
+
+  @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+  public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+    Speaker existingSpeaker = speakerRepository.getOne(id);
+    BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+    return speakerRepository.saveAndFlush(existingSpeaker);
   }
 }
